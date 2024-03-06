aee06b42df10d0b68909ef4b788aec7aab108e17
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-03-06 21:10:15 +0500

    Add PlaceLocation class to store location information

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -4,12 +4,22 @@ import "package:uuid/uuid.dart";
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Places {
-  Places({
-    required this.title,
-    required this.image,
-  }) : id = uuid.v4();
+  Places({required this.title, required this.image, required this.location})
+      : id = uuid.v4();
   final String title;
   final String id;
+  final PlaceLocation location;
   final File image;
 }
