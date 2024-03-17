26f923ebfee2cc1cd74a5b37d96dba81d4559fd4
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-03-17 12:52:22 +0500

    Refactor PlaceLocation and Places constructors

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -5,7 +5,7 @@ import "package:uuid/uuid.dart";
 const uuid = Uuid();
 
 class PlaceLocation {
-  PlaceLocation({
+  const PlaceLocation({
     required this.latitude,
     required this.longitude,
     required this.address,
@@ -16,8 +16,12 @@ class PlaceLocation {
 }
 
 class Places {
-  Places({required this.title, required this.image, required this.location})
-      : id = uuid.v4();
+  Places({
+    required this.title,
+    required this.image,
+    required this.location,
+    String? id,
+  }) : id = id ?? uuid.v4();
   final String title;
   final String id;
   final PlaceLocation location;
