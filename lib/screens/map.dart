a750a6ad86ff3a6d51a4d6c32c88a775426e0dc3
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-03-17 12:52:26 +0500

    Add MapScreen widget with Google Maps integration

---
+++ lib/screens/map.dart
@@ -0,0 +1,75 @@
+import 'package:favourite_place/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.initialLocation = const PlaceLocation(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: 'Googleplex',
+    ),
+    this.isSelecting = true,
+  });
+
+  final PlaceLocation initialLocation;
+  final bool isSelecting;
+  @override
+  State<StatefulWidget> createState() {
+    return _MapScreenState();
+  }
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(widget.isSelecting ? 'Pick your Location' : 'Your Location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () {
+                if (_pickedLocation == null) {
+                  return;
+                }
+                Navigator.of(context).pop(_pickedLocation);
+              },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        onTap: !widget.isSelecting
+            ? null
+            : (position) {
+                setState(() {
+                  _pickedLocation = position;
+                });
+              },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLocation.latitude,
+            widget.initialLocation.longitude,
+          ),
+          zoom: 16,
+        ),
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: _pickedLocation ??
+                      LatLng(
+                        widget.initialLocation.latitude,
+                        widget.initialLocation.longitude,
+                      ),
+                ),
+              },
+      ),
+    );
+  }
+}
